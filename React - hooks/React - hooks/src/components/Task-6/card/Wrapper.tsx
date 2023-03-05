bf6f62042849c8ba3199f813fe93fc69f1c20359
Mateusz Lipowicz <lipusek1997@gmail.com>
2023-03-05 19:48:25 +0100

    Deleted files

--- React - hooks/React - hooks/src/components/Task-6/card/Wrapper.tsx
+++
@@ -1,7 +0,0 @@
-type childrenProp = {
-    children: string | JSX.Element | JSX.Element[];
-};
-
-export const Wrapper = ({ children }: childrenProp) => {
-    return <div className="card-wrapper">{children}</div>;
-};
